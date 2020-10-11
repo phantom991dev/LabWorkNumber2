89c478ee03ecc25ac0babbc4bfb2aa1731fc6c08
phantom991dev <72074402+phantom991dev@users.noreply.github.com>
2020-10-11 20:16:53 +0600

    FULL TRUE CODE 100%

--- LabWorkNumber4/LabWorkNumber4/Program.cs
+++ LabWorkNumber4/LabWorkNumber4/Program.cs
@@ -5,9 +5,23 @@ namespace LabWorkNumber4
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Privet");
+        { 
+            string text = "1 - пример со знаком X \n" +
+                "2 - пример со знаком Y \n" +
+                "3 - пример с цифрами (без знаков X и Y) \n";
+
+            Console.Write(text + "Введите цифру 1,2,3 > ");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            switch(result)
+            {
+                case 1: { Console.WriteLine("z = 1 - x/2 + x^2/6 - x^3/24 + x^4/120"); break; }
+                case 2: { Console.WriteLine("z = 1 - y/2 + y^2/6 - y^3/24 + y^4/120"); break; }
+                case 3: { Console.WriteLine("z = 1 - 2 + 2/6 - 3/24 + 4/120"); break; }
+                default: { Console.WriteLine("Неверное значение"); break; }
+            }
+
+            //z = 1 - x / 2 + y ^ 2 / 6 - x ^ 3 / 24 + y ^ 4 / 120
+            }
         }
     }
-}
